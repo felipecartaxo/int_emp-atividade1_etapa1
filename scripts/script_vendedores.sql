483ab2241dabd5834789f10c4f918550dd1bb77a
Felipe Cartaxo de Freitas <107081472+felipecartaxo@users.noreply.github.com>
2026-03-05 22:50:35 -0300

    refactor: ajusta a forma que as tabelas serao criadas

--- scripts/script_vendedores.sql
+++
@@ -1,6 +0,0 @@
--- Popula a tabela de vendedores
-CREATE TABLE vendedores (
-    vendedor_id   INTEGER      NOT NULL,
-    vendedor_nome VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_vendedores PRIMARY KEY (vendedor_id)
-);
